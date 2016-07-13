ec13e05651ea98ed3b1a2b18224261e651eabc19
Thomas Desrosiers <thomas@databricks.com>
2016-07-13 16:28:04 -0700

    Adds two new storage implementations, regex and multistoage

---
+++ storage/multistorage.go
@@ -0,0 +1,17 @@
+package storage
+
+// A storage that will query multiple storages that will only return an ErrShortNotSet if the short code doens't exist in all of them
+type MultiStorage []Storage
+
+func (s *MultiStorage) Load(short string) (string, error) {
+	for _, store := range *s {
+		s, err := store.Load(short)
+		if err == ErrShortNotSet {
+			continue
+		}
+
+		return s, err
+	}
+
+	return "", ErrShortNotSet
+}
---
+++ storage/regex.go
@@ -0,0 +1,41 @@
+package storage
+
+import "regexp"
+
+type remap struct {
+	Regex       *regexp.Regexp
+	Replacement string
+}
+
+type Regex struct {
+	remaps []remap
+}
+
+func NewRegexFromList(redirects map[string]string) (*Regex, error) {
+	remaps := make([]remap, 0, len(redirects))
+
+	for regexString, redirect := range redirects {
+		r, err := regexp.Compile(regexString)
+		if err != nil {
+			return nil, err
+		}
+		remaps = append(remaps, remap{
+			Regex:       r,
+			Replacement: redirect,
+		})
+	}
+
+	return &Regex{
+		remaps: remaps,
+	}, nil
+}
+
+func (r Regex) Load(short string) (string, error) {
+	for _, remap := range r.remaps {
+		if remap.Regex.MatchString(short) {
+			return remap.Regex.ReplaceAllString(short, remap.Replacement), nil
+		}
+	}
+
+	return "", ErrShortNotSet
+}
