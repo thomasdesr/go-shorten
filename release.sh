a6e8f4fc8991a1230f5788237e0fcbf19945a6dc
Thomas Desrosiers <thomas@databricks.com>
2017-04-04 17:23:02 -0700

    Adds a super basic release building script

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Builds our release artifacts and puts
+
+cp -r static $TMPDIR
+
+export GOOS GOARCH
+for GOOS in linux darwin; do
+    for GOARCH in amd64 arm; do
+        echo "Building $GOOS-$GOARCH"
+        TMPDIR="$(mktemp -d)"
+
+        cp -r static $TMPDIR
+        go build -o $TMPDIR/go-shorten github.com/thomaso-mirodin/go-shorten
+
+        tar -cf go-shorten-$GOOS-$GOARCH.tar \
+            -C $TMPDIR \
+            go-shorten static
+
+        rm -r $TMPDIR
+    done
+done
+
+echo "Release complete"
