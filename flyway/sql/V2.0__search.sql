87fc1c25c3013fd81470f84a308a026252b90fe3
Thomas Desrosiers <thomas@databricks.com>
2018-04-11 14:04:03 -0700

    Fix v2 we forgot to create the extensions

--- flyway/sql/V2.0__search.sql
+++ flyway/sql/V2.0__search.sql
@@ -1,2 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+
 CREATE INDEX links_trgm_idx ON links USING gin (link gin_trgm_ops);
 CREATE INDEX urls_trgm_idx ON urls USING gin (url gin_trgm_ops);
